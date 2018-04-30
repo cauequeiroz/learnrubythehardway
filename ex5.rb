e0b6f5e115dee4104b65aba594128d9e3d53e7e9
Caue Queiroz <cauenqueiroz@gmail.com>
2018-04-30 03:33:39 -0300

    Finish ex 5

---
+++ ex5.rb
@@ -0,0 +1,23 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}"
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# Study Drills
+
+# Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby.
+
+puts "#{height} inches -> #{height * 2.54} centimeters."
+puts "#{weight} pounds -> #{weight * 0.45} kilograms."
