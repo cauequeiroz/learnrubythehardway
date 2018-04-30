58b9456f913fc42019914dac2eed39603acf118a
Caue Queiroz <cauenqueiroz@gmail.com>
2018-04-30 14:02:54 -0300

    Finish ex 11

---
+++ ex11.rb
@@ -0,0 +1,10 @@
+print "How old are you? "
+age = gets.chomp.to_i
+
+print "How tall are you? "
+height = gets.chomp.to_f
+
+print "How much do you weight? "
+weight = gets.chomp.to_f
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
